94fcd8b7ef1ba6576e0e96c3389058fa7f7cf39a
starhih <starhi@starhiherbs.com>
2025-05-07 12:45:50 +0530

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import HeroSection from '@/components/home/HeroSection';
 import AboutIntroSection from '@/components/home/AboutIntroSection';
 import StatisticsSection from '@/components/home/StatisticsSection';
 import FeaturedHighlights from '@/components/home/FeaturedHighlights';
-import FeaturedProduct from '@/components/home/FeaturedProduct';
-import NewsUpdates from '@/components/home/NewsUpdates';
 import CertificationCarousel from '@/components/home/CertificationCarousel';
 import ProductCategories from '@/components/home/ProductCategories';
 import WhyChooseUs from '@/components/home/WhyChooseUs';
@@ -19,8 +17,6 @@ export default function Home() {
       <HeroSection />
       <AboutIntroSection />
       <FeaturedHighlights />
-      <FeaturedProduct />
-      <NewsUpdates />
       <CertificationCarousel />
       <ProductCategories />
       <WhyChooseUs />
